c37ee70c4e16ccf269ed7bbef1cf0023b2a1cce4
Roman Heinrich <roman.heinrich@gmail.com>
2013-07-07 11:08:26 +0200

    Squashed 'github.com/iansrobinson/neo4j-good-practices/' content from commit 075de2d
    git-subtree-dir: github.com/iansrobinson/neo4j-good-practices
    git-subtree-split: 075de2d406bf2f57d3897b7745629ea9e4a245ce

---
+++ src/main/java/org/neo4j/good_practices/ColleagueFinder.java
@@ -0,0 +1,84 @@
+package org.neo4j.good_practices;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.neo4j.cypher.javacompat.ExecutionEngine;
+import org.neo4j.cypher.javacompat.ExecutionResult;
+import org.neo4j.graphdb.GraphDatabaseService;
+
+public class ColleagueFinder
+{
+    private final ExecutionEngine cypherEngine;
+
+    public ColleagueFinder( GraphDatabaseService db )
+    {
+        this.cypherEngine = new ExecutionEngine( db );
+    }
+
+    public Iterator<Map<String, Object>> findColleaguesFor( String name )
+    {
+        String cypher =
+                "MATCH (company)<-[:WORKS_FOR]-(me:person)-[:HAS_SKILL]->(skill),\n" +
+                        "      (company)<-[:WORKS_FOR]-(colleague)-[:HAS_SKILL]->(skill)\n" +
+                        "WHERE  me.name = {name}\n" +
+                        "RETURN colleague.name AS name,\n" +
+                        "       count(skill) AS score,\n" +
+                        "       collect(skill.name) AS skills\n" +
+                        "ORDER BY score DESC";
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put( "name", name );
+
+        ExecutionResult result = cypherEngine.execute( cypher, params );
+        System.out.println( result.dumpToString() );
+
+        return cypherEngine.execute( cypher, params ).iterator();
+    }
+
+    public Iterator<Map<String, Object>> findPeopleFor( String name )
+    {
+        String cypher =
+                "MATCH (me:person)-[:HAS_SKILL]->(skill),\n" +
+                        "      (company)<-[:WORKS_FOR]-(person)-[:HAS_SKILL]->(skill)\n" +
+                        "WHERE  me.name = {name}\n" +
+                        "RETURN person.name AS name,\n" +
+                        "       company.name AS company,\n" +
+                        "       count(skill) AS score,\n" +
+                        "       collect(skill.name) AS skills\n" +
+                        "ORDER BY score DESC";
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put( "name", name );
+
+        ExecutionResult result = cypherEngine.execute( cypher, params );
+        System.out.println( result.dumpToString() );
+
+        return cypherEngine.execute( cypher, params ).iterator();
+    }
+
+    public Iterator<Map<String, Object>> findWithMatchingSkills( String name, String... skills )
+    {
+        String cypher = "MATCH p=(me:person)-[:WORKED_ON*2..4]-\n" +
+                "        (person)-[:HAS_SKILL]->(skill)\n" +
+                "WHERE me.name = {name}\n" +
+                "      AND person <> me \n" +
+                "      AND skill.name IN {skills}\n" +
+                "WITH person, skill, min(length(p)) as pathLength\n" +
+                "RETURN person.name AS name,\n" +
+                "       count(skill) AS score,\n" +
+                "       collect(skill.name) AS skills,\n" +
+                "       ((pathLength - 1)/2) AS distance\n" +
+                "ORDER BY score DESC;";
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put( "name", name );
+        params.put( "skills", skills );
+
+        ExecutionResult result = cypherEngine.execute( cypher, params );
+        System.out.println( result.dumpToString() );
+
+        return cypherEngine.execute( cypher, params ).iterator();
+    }
+}
---
+++ src/test/java/org/neo4j/good_practices/ColleagueFinderTest.java
@@ -0,0 +1,48 @@
+package org.neo4j.good_practices;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.test.TestGraphDatabaseFactory;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class ColleagueFinderTest
+{
+    private static GraphDatabaseService db;
+    private static ColleagueFinder finder;
+
+    @BeforeClass
+    public static void init()
+    {
+        db = new TestGraphDatabaseFactory().newImpermanentDatabase();
+        ExampleGraph.populateWithSmallGraph( db );
+
+        finder = new ColleagueFinder( db );
+    }
+
+    @AfterClass
+    public static void shutdown()
+    {
+        db.shutdown();
+    }
+
+    @Test
+    public void shouldFindColleaguesWithSimilarSkills() throws Exception
+    {
+        // when
+        Iterator<Map<String, Object>> results = finder.findColleaguesFor( "Ian" );
+
+        // then
+        assertEquals( "Lucy", results.next().get( "name" ) );
+        assertEquals( "Bill", results.next().get( "name" ) );
+
+        assertFalse( results.hasNext() );
+    }
+}
